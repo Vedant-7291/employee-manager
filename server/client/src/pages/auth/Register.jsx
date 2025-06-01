324cc83c35399d415cc823256c6af6077bef5590
Vedant-7291 <vedant115619@gmail.com>
2025-06-01 10:58:10 +0530

    Update Register.jsx

--- server/client/src/pages/auth/Register.jsx
+++ server/client/src/pages/auth/Register.jsx
@@ -26,7 +26,7 @@ const Register = () => {
     setIsLoading(true);
     
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/register', formData);
+      const response = await axios.post('http://employee-manager-11.com/api/auth/register', formData);
       
       if (response.data.user && response.data.token) {
         setUser({
